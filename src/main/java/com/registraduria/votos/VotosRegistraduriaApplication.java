a3b4c270b80f2bd10cc4cd8615b8468c8a2fcfb4
winstonjss <jhonatan_15-01@hotmail.com>
2023-09-10 19:20:31 -0500

    Creacion de cruds

---
+++ src/main/java/com/registraduria/votos/VotosRegistraduriaApplication.java
@@ -0,0 +1,13 @@
+package com.registraduria.votos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotosRegistraduriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotosRegistraduriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/registraduria/votos/VotosRegistraduriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registraduria.votos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotosRegistraduriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
